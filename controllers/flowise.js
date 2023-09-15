07d278230433df738c2ae83fe6461457ce191cd0
YsisC <ysisclongart@gmail.com>
2023-09-15 15:27:14 -0300

    install flowise conection

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,22 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    //call the Flowise Endpoint
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`,
+          "Content-Type": "application/json"
+      },
+        body: JSON.stringify(flowiseData)
+    }
+    );
+    const data = await response.json();
+
+    console.log(data)
+    res.status(200).json({ message: data });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
